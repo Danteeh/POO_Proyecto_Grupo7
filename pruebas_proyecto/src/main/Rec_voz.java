7766db888c447bfb306404058a2bcfd29a826ae9
Miguel Chavez <boingochavez1@gmail.com>
2020-11-11 18:19:05 -0500

    Se creo una clase con todo la configuracion para reconocer la voz

---
+++ pruebas_proyecto/src/main/Rec_voz.java
@@ -0,0 +1,61 @@
+package main;
+
+import java.util.concurrent.Future;
+import com.microsoft.cognitiveservices.speech.*;
+
+public class Rec_voz {
+	public static void main(String[] args) {
+	       try {
+	            //Llave de subscripcion
+	            String speechSubscriptionKey = "YourSubscriptionKey";
+	            
+	            //Region de cloud computing que nos da el servicio
+	            String serviceRegion = "westus";
+
+	            int exitCode = 1;
+	            SpeechConfig config = SpeechConfig.fromSubscription(speechSubscriptionKey, serviceRegion);
+	            assert(config != null);
+
+	            SpeechRecognizer reco = new SpeechRecognizer(config);
+	            assert(reco != null);
+
+	            System.out.println("Di algo");
+
+	            Future<SpeechRecognitionResult> task = reco.recognizeOnceAsync();
+	            assert(task != null);
+
+	            SpeechRecognitionResult result = task.get();
+	            assert(result != null);
+
+	            if (result.getReason() == ResultReason.RecognizedSpeech) {
+	                System.out.println("We recognized: " + result.getText());
+	                exitCode = 0;
+	            }
+	            else if (result.getReason() == ResultReason.NoMatch) {
+	                System.out.println("NOMATCH: Speech could not be recognized.");
+	            }
+	            else if (result.getReason() == ResultReason.Canceled) {
+	                CancellationDetails cancellation = CancellationDetails.fromResult(result);
+	                System.out.println("CANCELED: Reason=" + cancellation.getReason());
+
+	                if (cancellation.getReason() == CancellationReason.Error) {
+	                    System.out.println("CANCELED: ErrorCode=" + cancellation.getErrorCode());
+	                    System.out.println("CANCELED: ErrorDetails=" + cancellation.getErrorDetails());
+	                    System.out.println("CANCELED: Did you update the subscription info?");
+	                }
+	            }
+
+	            reco.close();
+
+	            System.exit(exitCode);
+	        } catch (Exception ex) {
+	            System.out.println("Unexpected exception: " + ex.getMessage());
+
+	            assert(false);
+	            System.exit(1);
+	        }
+	    }
+	
+	
+
+}
